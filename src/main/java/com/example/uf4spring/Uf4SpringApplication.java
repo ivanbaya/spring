a6501f6e24b8286be1dd9289bfa3f6e1ca000455
BuildTools <unconfigured@null.spigotmc.org>
2022-01-13 17:38:55 +0100

    mapping

--- src/main/java/com/example/uf4spring/Uf4SpringApplication.java
+++ src/main/java/com/example/uf4spring/Uf4SpringApplication.java
@@ -3,6 +3,7 @@ package com.example.uf4spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 public class Uf4SpringApplication {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     @ResponseBody
     String hola(String nom) {
         return "Hola !"+nom;
