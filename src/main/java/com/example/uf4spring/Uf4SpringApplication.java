eb5755d52d85cf8e71e103fe400c13f12482f058
BuildTools <unconfigured@null.spigotmc.org>
2022-01-13 18:25:55 +0100

    h1 azul

--- src/main/java/com/example/uf4spring/Uf4SpringApplication.java
+++ src/main/java/com/example/uf4spring/Uf4SpringApplication.java
@@ -4,18 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SpringBootApplication
 public class Uf4SpringApplication {
 
-    @GetMapping("/")
+    @GetMapping("/inicio")
     @ResponseBody
-    String hola() {
-        return "Hola Ivan!";
-    }
+    public void hola() { }
 
     @GetMapping("/imprimirArray")
     @ResponseBody
