87b3ab22c760bd71653a83fa21c435c3f48ab532
BuildTools <unconfigured@null.spigotmc.org>
2022-01-13 17:15:30 +0100

    first comit

---
+++ src/main/java/com/example/uf4spring/Uf4SpringApplication.java
@@ -0,0 +1,22 @@
+package com.example.uf4spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class Uf4SpringApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String hola(String nom) {
+        return "Hola !"+nom;
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Uf4SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uf4spring/Uf4SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uf4spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uf4SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
