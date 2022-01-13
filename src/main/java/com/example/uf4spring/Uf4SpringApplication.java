18cc7e7d0cb3f527c9e3b8e85f277e6335e4279c
BuildTools <unconfigured@null.spigotmc.org>
2022-01-13 18:34:02 +0100

    h1 azul

--- src/main/java/com/example/uf4spring/Uf4SpringApplication.java
+++ src/main/java/com/example/uf4spring/Uf4SpringApplication.java
@@ -3,16 +3,22 @@ package com.example.uf4spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @SpringBootApplication
 public class Uf4SpringApplication {
 
-    @GetMapping("/inicio")
-    @ResponseBody
-    public void hola() { }
+    @RequestMapping("/inicio")
+    public ModelAndView hola() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("inicio.html");
+        return modelAndView;
+    }
 
     @GetMapping("/imprimirArray")
     @ResponseBody
